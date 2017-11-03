d27d250f4bd8f85c29da55f053e134c8d2b2a6df
Ariel Matos <dominicanyor809@gmail.com>
2017-11-03 17:10:25 -0400

    Adding back some brew install scripts

--- brew.sh
+++ brew.sh
@@ -3,51 +3,54 @@
 # Install command-line tools using Homebrew.
 
 # Make sure we’re using the latest Homebrew.
-# brew update
+brew update
 
 # Upgrade any already-installed formulae.
-# brew upgrade
+brew upgrade
 
 # Install GNU core utilities (those that come with macOS are outdated).
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-# brew install coreutils
+brew install coreutils
 
 # Install some other useful utilities like `sponge`.
 # brew install moreutils
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
-# brew install findutils
+brew install findutils
 # Install GNU `sed`, overwriting the built-in `sed`.
-# brew install gnu-sed --with-default-names
+brew install gnu-sed --with-default-names
 # Install Bash 4.
 # Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
 # running `chsh`.
-# brew install bash
-# brew install bash-completion2
+brew install bash
+brew install bash-completion2
 
 # Switch to using brew-installed bash as default shell
-# if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
-#   echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
-#   chsh -s /usr/local/bin/bash;
-# fi;
+if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
+  echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
+  chsh -s /usr/local/bin/bash;
+fi;
 
 # Install `wget` with IRI support.
-# brew install wget --with-iri
+brew install wget --with-iri
 
 # Install GnuPG to enable PGP-signing commits.
 # brew install gnupg
 
 # Install more recent versions of some macOS tools.
-# brew install vim --with-override-system-vi
-# brew install grep
-# brew install openssh
-# brew install screen
+brew install vim --with-override-system-vi
+brew install grep
+brew install openssh
+brew install screen
+brew install homebrew/php/php71
+brew install composer
+brew install mysql
 # brew install homebrew/php/php56 --with-gmp
 
 # Install font tools.
-# brew tap bramstein/webfonttools
-# brew install sfnt2woff
-# brew install sfnt2woff-zopfli
-# brew install woff2
+brew tap bramstein/webfonttools
+brew install sfnt2woff
+brew install sfnt2woff-zopfli
+brew install woff2
 
 # Install some CTF tools; see https://github.com/ctfs/write-ups.
 # brew install aircrack-ng
@@ -94,4 +97,4 @@
 # brew install zopfli
 
 # Remove outdated versions from the cellar.
-# brew cleanup
+brew cleanup
